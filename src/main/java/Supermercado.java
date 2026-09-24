a407be49d4bb6978709e1d44e0c93f6c8a2a50f6
Pablo Cubillos <pabloa.cubillosf@uqvirtual.edu.co>
2026-09-24 09:11:57 -0500

    Creación supermercado y listas de él

---
+++ src/main/java/Supermercado.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Supermercado {
+    private String nombre;
+    private String direccion;
+    private int telefono;
+
+    //creacion de listas
+private List<Producto> listaProductos;
+private List<Compra> listaCompras;
+private List<Cliente> listaClientes;
+
+
+    public Supermercado(String nombre, String direccion, int telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+
+        //inicializar listas
+        listaProductos = new ArrayList<>();
+        listaCompras = new ArrayList<>();
+        listaClientes = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(List<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public List<Compra> getListaCompras() {
+        return listaCompras;
+    }
+
+    public void setListaCompras(List<Compra> listaCompras) {
+        this.listaCompras = listaCompras;
+    }
+
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    @Override
+    public String toString() {
+        return "Supermercado{" +
+                "nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono=" + telefono + ", lista de productos="
+                + listaProductos+", lista de compras=" + listaCompras +
+                ", lista de clientes=" + listaClientes+
+                '}';
+    }
+}
