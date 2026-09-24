73cfbed2908da247b34f9f66b33dc667559c8dbc
Felipe Avendano <felipe.lopeza@uniquindio.edu.co>
2026-09-23 22:02:37 -0500

    Base de la Aplicación

--- src/main/java/Aplicacion.java
+++ src/main/java/Aplicacion.java
@@ -1,6 +1,38 @@
-public class Aplicacion {
-public static void main(String[]args){
-    System.out.println("HOla");
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.logging.SocketHandler;
 
+
+public class Aplicacion {
+    public static void main (String[] args) {
+
+        // Menu interactivo
+        Scanner sc = new Scanner(System.in);
+
+int opcion;
+do{
+    System.out.println("====== MENU Supermercado======");
+    System.out.println("1. ");
+
+
+    System.out.print("Seleccione la opción: ");
+    opcion = sc.nextInt();
+    sc.nextLine();
+
+    switch (opcion){
+        case 1:
     }
+
+
+
+} while (opcion != 0);
+
+
+
+
+
+
+        sc.close();
+    }
+
 }
