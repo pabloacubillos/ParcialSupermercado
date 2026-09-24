836a33c01eeb38b4b1b24d8101aa27e27f9bae8d
Santiago Alejandro Triviño Correa <santitrivi0211@gmail.com>
2026-09-23 21:01:57 -0500

    feat: creacion de la clase enum MetodoPago, su integracion en la clase Compra y la creacion de getters, setter y toString de la clase Compra

--- src/main/java/Compra.java
+++ src/main/java/Compra.java
@@ -15,4 +15,55 @@ public class Compra {
         this.correo = correo;
         this.metodoPago = metodoPago;
     }
+
+    @Override
+    public String toString() {
+        return "Compra{" +
+                "codigo='" + codigo + '\'' +
+                ", fecha=" + fecha +
+                ", telefano='" + telefano + '\'' +
+                ", correo='" + correo + '\'' +
+                ", metodoPago=" + metodoPago +
+                '}';
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTelefano() {
+        return telefano;
+    }
+
+    public void setTelefano(String telefano) {
+        this.telefano = telefano;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
 }
