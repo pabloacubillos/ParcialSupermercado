4cceac84c33cf9b613dd171cc5267b3fb2041aba
Felipe Avendano <felipe.lopeza@uniquindio.edu.co>
2026-09-23 21:44:58 -0500

    Clase Cliente

--- src/main/java/Aplicacion.java
+++ src/main/java/Aplicacion.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Aplicacion {
 public static void main(String[]args){
     System.out.println("HOla");
 
--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,3 +1,34 @@
 public class Cliente {
-    private 
+    private String nombre;
+    private String documento;
+    private int telefono;
+    private String correo;
+
+    public Cliente(String nombre, String documento, int telefono, String correo) {
+        this.nombre = nombre;
+        this.documento = documento;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public String getDocumento() {return documento;}
+    public void setDocumento(String documento) {this.documento = documento;}
+
+    public int getTelefono() {return telefono;}
+    public void setTelefono(int telefono) {this.telefono = telefono;}
+
+    public String getCorreo() {return correo;}
+    public void setCorreo(String correo) {this.correo = correo;}
+
+    @Override
+    public String toString() {
+        return "\nCliente: " +
+                "nombre='" + nombre + '\'' +
+                ", documento='" + documento + '\'' +
+                ", telefono=" + telefono +
+                ", correo='" + correo + '\'' ;
+    }
 }
